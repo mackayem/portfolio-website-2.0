dc2ef46d8724c60e9974b791b358c99e87062800
Emily Mackay <>
2021-01-25 22:04:11 -0800

    adds create-react-app build

---
+++ portfolio-website/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+// ---- old-style
+// class App extends Component {
+//   render() {
+//     return (
+//
+//     );
+//   }
+// }
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Emily Mackay</h1>
+    </div>
+  );
+}
+
+export default App;
