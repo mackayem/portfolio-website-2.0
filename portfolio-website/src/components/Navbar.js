5968ee026dd23d7d190f90e1cc153951b42e169c
Emily Mackay <>
2021-01-31 18:49:00 -0800

    adds styled bootstrap navbar with toggle ability

--- portfolio-website/src/components/Navbar.js
+++ portfolio-website/src/components/Navbar.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Navbar () {
   return (
-    // <div className="Navbar">
-      <nav className="navbar navbar-dark bg-dark navbar-expand-md">
+    <div className="navbar-wrapper">
+      <nav className="navbar navbar-expand-md navbar-light">
         <a className="navbar-brand" href="#">Emily Mackay</a>
 
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation menu">
@@ -12,16 +12,16 @@ function Navbar () {
 
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
+            <li className="nav-item mx-2">
               <a className="nav-link" href="#about-section">About</a>
             </li>
-            <li className="nav-item">
+            <li className="nav-item mx-2">
               <a className="nav-link" href="#education-section">Education</a>
             </li>
-            <li className="nav-item">
+            <li className="nav-item mx-2">
               <a className="nav-link" href="#projects-section">Projects</a>
             </li>
-            <li className="nav-item">
+            <li className="nav-item mx-2">
               <a className="nav-link" href="#contact-section">Contact</a>
             </li>
           </ul>
@@ -30,7 +30,7 @@ function Navbar () {
 
 
       </nav>
-    // </div>
+    </div>
   )
 };
 
